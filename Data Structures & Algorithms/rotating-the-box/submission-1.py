7ced986be18febd408245e04ac6e56910c5c307d
AKBarcenas <9867993+AKBarcenas@users.noreply.github.com>
2026-03-29 22:45:44 +0000

    Add: rotating-the-box - submission-1

---
+++ Data Structures & Algorithms/rotating-the-box/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
+        rows = len(boxGrid)
+        cols = len(boxGrid[0])
+
+        for rowInd in range(rows):
+            nextSpot = cols - 1
+            for colInd in range(cols - 1, -1, -1):
+                if boxGrid[rowInd][colInd] == '#':
+                    tmp = boxGrid[rowInd][nextSpot]
+                    boxGrid[rowInd][nextSpot] = boxGrid[rowInd][colInd]
+                    boxGrid[rowInd][colInd] = tmp
+                    nextSpot -= 1
+                if boxGrid[rowInd][colInd] == '*':
+                    nextSpot = colInd - 1
+        
+        rotated = []
+        for _ in range(cols):
+            rotated.append(['.'] * rows)
+        
+        for colInd in range(cols):
+            for rowInd in range(rows - 1, -1 ,-1):
+                rotated[colInd][rows - 1 - rowInd] = boxGrid[rowInd][colInd]
+
+        return rotated
