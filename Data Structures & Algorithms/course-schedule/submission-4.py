32ecedf1ab78b0e799d8e0f337f160a4dff5449b
AKBarcenas <9867993+AKBarcenas@users.noreply.github.com>
2026-03-30 20:02:02 +0000

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        nodes = defaultdict(list)
+        for first, second in prerequisites:
+            nodes[first].append(second)
+
+        visited = set()
+        def traverse(node):
+            if node in visited:
+                return False
+            if not node in nodes or not nodes[node]:
+                return True
+            visited.add(node)
+            for neighbor in nodes[node]:
+                if not traverse(neighbor):
+                    return False
+            nodes[node] = []
+            visited.remove(node)
+            return True
+
+        for node in nodes:
+            if not traverse(node):
+                return False
+
+
+        return True
