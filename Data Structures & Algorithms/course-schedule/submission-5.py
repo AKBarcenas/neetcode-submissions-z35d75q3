4c4aa0d70125ba6929a1be4f7b7d5aaa8664571d
AKBarcenas <9867993+AKBarcenas@users.noreply.github.com>
2026-03-30 20:51:36 +0000

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courseMap = defaultdict(list)
+        for c1, c2 in prerequisites:
+            courseMap[c1].append(c2)
+
+        visited = set()
+        def traverse(course):
+            if course in visited:
+                return False
+            if not course in courseMap or not courseMap[course]:
+                return True
+
+            visited.add(course)
+            neighbors = courseMap[course]
+            for neighbor in neighbors:
+                if not traverse(neighbor):
+                    return False
+            courseMap[course] = []
+            visited.remove(course)
+            return True
+
+        for course in courseMap.keys():
+            if not traverse(course):
+                return False
+        return True
