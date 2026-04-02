7eff07e55e3959afae53a4179afd0f526178719f
AKBarcenas <9867993+AKBarcenas@users.noreply.github.com>
2026-04-02 00:09:06 +0000

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+        cache = {}
+
+        def traverse(i):
+            if i == len(s):
+                return True
+            if i in cache:
+                return cache[i]
+            possible = False
+            #for j in range(i, len(s)):
+                #if s[i:j+1] in wordSet:
+                    #possible = possible or traverse(j + 1)
+            for word in wordDict:
+                if s[i:i + len(word)] == word:
+                    possible = possible or traverse(i + len(word))
+            cache[i] = possible
+            return possible
+        return traverse(0)
