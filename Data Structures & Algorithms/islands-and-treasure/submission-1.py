481b26517426713b8b72d76b05c40d78837038cc
AKBarcenas <9867993+AKBarcenas@users.noreply.github.com>
2026-04-06 23:07:46 +0000

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.py
@@ -0,0 +1,44 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        def outOfBounds(row, col):
+            if row < 0 or col < 0:
+                return True
+            if row >= len(grid) or col >= len(grid[0]):
+                return True
+            return False
+
+        """
+            def traverse(row, col, count):
+            if outOfBounds(row, col) or grid[row][col] < count:
+                return
+
+            grid[row][col] = count
+            directions = [(0,-1), (0,1), (-1,0), (1,0)]
+            for direction in directions:
+                traverse(row + direction[0], col + direction[1], count + 1)
+        """
+        current = []
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 0:
+                    current.append((row, col))
+                    
+        count = 0
+        visited = set()
+        while current:
+            nextLayer = []
+            for pos in current:
+                row, col = pos[0], pos[1]
+                if outOfBounds(row, col) or pos in visited or grid[row][col] == -1:
+                    continue
+                visited.add(pos)
+                grid[row][col] = count
+
+                directions = [(0,-1), (0,1), (-1,0), (1,0)]
+                for direction in directions:
+                    nextLayer.append((row + direction[0], col + direction[1]))
+                
+            count += 1
+            current = nextLayer
+                
+                
